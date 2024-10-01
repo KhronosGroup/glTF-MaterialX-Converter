4ce474c3aeb2088c5751ff93a114b5340c9fef0e
Bernard Kwok <kwokcb@gmail.com>
2024-10-01 14:38:19 -0400

    Add in required graph config documentation.

--- documents/html/search/all_3.js
+++ documents/html/search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['data_0',['Sample Data',['../index.html#autotoc_md8',1,'']]],
+  ['data_0',['Sample Data',['../index.html#autotoc_md9',1,'']]],
   ['dependencies_1',['Dependencies',['../index.html#autotoc_md2',1,'']]],
   ['documentation_2',['Documentation',['../index.html#autotoc_md5',1,'']]]
 ];
--- documents/html/search/all_5.js
+++ documents/html/search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['interface_0',['Command Line Interface',['../index.html#autotoc_md4',1,'']]],
+  ['interfaces_0',['Command Line Interfaces',['../index.html#autotoc_md4',1,'']]],
   ['interop_1',['MaterialX / glTF Procedurals Interop',['../index.html',1,'']]],
   ['introduction_2',['Introduction',['../index.html#autotoc_md1',1,'']]]
 ];
