54f1b3176abc64f8a215b07b8114e9aae835bdb9
Bernard Kwok <kwokcb@gmail.com>
2024-10-01 14:40:15 -0400

    Add in required graph config documentation. (#9)

--- documents/html/search/all_2.js
+++ documents/html/search/all_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['command_20line_20interface_0',['Command Line Interface',['../index.html#autotoc_md4',1,'']]],
-  ['converter_2epy_1',['converter.py',['../converter_8py.html',1,'']]],
-  ['createworkingdocument_2',['createWorkingDocument',['../namespacegltf__materialx__converter_1_1converter.html#a84eb94df87934d387d021d9dfb0a97da',1,'gltf_materialx_converter::converter']]]
+  ['command_20line_20interfaces_0',['Command Line Interfaces',['../index.html#autotoc_md4',1,'']]],
+  ['configurations_1',['Supported MaterialX Configurations',['../index.html#autotoc_md8',1,'']]],
+  ['converter_2epy_2',['converter.py',['../converter_8py.html',1,'']]],
+  ['createworkingdocument_3',['createWorkingDocument',['../namespacegltf__materialx__converter_1_1converter.html#a84eb94df87934d387d021d9dfb0a97da',1,'gltf_materialx_converter::converter']]]
 ];
--- documents/html/search/all_6.js
+++ documents/html/search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['line_20interface_0',['Command Line Interface',['../index.html#autotoc_md4',1,'']]],
+  ['line_20interfaces_0',['Command Line Interfaces',['../index.html#autotoc_md4',1,'']]],
   ['loadstandardlibraries_1',['loadStandardLibraries',['../namespacegltf__materialx__converter_1_1converter.html#ac9217902289c0172ed44ab9debd9873c',1,'gltf_materialx_converter::converter']]],
   ['logger_2',['logger',['../classgltf__materialx__converter_1_1converter_1_1gl_t_f_material_x_converter.html#a76d4a55f730d618932d8f4b1156010ab',1,'gltf_materialx_converter::converter::glTFMaterialXConverter']]]
 ];
