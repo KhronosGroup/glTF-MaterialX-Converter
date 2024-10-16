1c9e195a22cdf7aa26dc7072501f7c8850397c0e
Bernard Kwok <kwokcb@gmail.com>
2024-10-16 11:46:54 -0400

    Ad notebook. Fix up docs formatting.

--- documents/html/search/all_5.js
+++ documents/html/search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['example_0',['API Example',['../index.html#autotoc_md13',1,'']]],
+  ['example_0',['API Example',['../index.html#api-example',1,'']]],
   ['ext_5ftexture_5fprocedurals_5fmx_5f1_5f39_1',['EXT_TEXTURE_PROCEDURALS_MX_1_39',['../namespacegltf__materialx__converter_1_1converter.html#a5da61c62801b47864293c1b1bf234325',1,'gltf_materialx_converter::converter']]]
 ];
---
+++ documents/html/search/files_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['utilities_2epy_0',['utilities.py',['../utilities_8py.html',1,'']]],
+  ['utilities_2freadme_2emd_1',['README.md',['../utilities_2_r_e_a_d_m_e_8md.html',1,'']]]
+];
