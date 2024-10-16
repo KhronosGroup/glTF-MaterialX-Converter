5d7bcc9b633845c04b5aca185aa8e20dbf401e08
Bernard Kwok <kwokcb@gmail.com>
2024-10-16 16:00:01 -0400

    Html pages (#18)
    * Basic File Texture Support (#2)
    
    * - Add basic texture / image append if a file reference is found.
    - Add simple file example.
    
    * - Cleanup constant strings.
    - Extract out fallback texture logic to addFallbackTexture() method.
    - Normalize variable naming.
    
    * More cleanup. Remove python build scripts as using shell scripts.
    
    * Docs cleanup / CI cleanup (#4)
    
    * Ad notebook. Fix up docs formatting.
    
    * Update CI for github pages.
    
    * Fix CI
    
    * Check if replace string is being set.
    
    * Try fixing up doxygen version.
    
    * Revert doxygen change. Don't build docs in CI.
    
    * Fix linkages for html pages to main repo.
    
    * Update html.
    
    * Docs sync

--- documents/html/search/all_6.js
+++ documents/html/search/all_6.js
@@ -2,12 +2,11 @@ var searchData=
 [
   ['get_5ffiles_0',['get_files',['../namespacegltf__materialx__converter_1_1utilities.html#a51dd9b8edabf54ec5e99af0a45757aff',1,'gltf_materialx_converter::utilities']]],
   ['get_5fmetadata_1',['get_metadata',['../classgltf__materialx__converter_1_1converter_1_1gl_t_f_material_x_converter.html#abef23991acda9c9101d7391e1e3dbc6e',1,'gltf_materialx_converter::converter::glTFMaterialXConverter']]],
-  ['github_2',['Github',['../index.html#github',1,'']]],
-  ['gltf_20procedurals_20interop_3',['MaterialX / glTF Procedurals Interop',['../index.html',1,'']]],
-  ['gltf_5fmaterialx_5fconverter_4',['gltf_materialx_converter',['../namespacegltf__materialx__converter.html',1,'']]],
-  ['gltf_5fmaterialx_5fconverter_3a_3a_5f_5fmain_5f_5f_5',['__main__',['../namespacegltf__materialx__converter_1_1____main____.html',1,'gltf_materialx_converter']]],
-  ['gltf_5fmaterialx_5fconverter_3a_3aconverter_6',['converter',['../namespacegltf__materialx__converter_1_1converter.html',1,'gltf_materialx_converter']]],
-  ['gltf_5fmaterialx_5fconverter_3a_3amaterialx_5fto_5fgltf_7',['materialx_to_gltf',['../namespacegltf__materialx__converter_1_1materialx__to__gltf.html',1,'gltf_materialx_converter']]],
-  ['gltf_5fmaterialx_5fconverter_3a_3autilities_8',['utilities',['../namespacegltf__materialx__converter_1_1utilities.html',1,'gltf_materialx_converter']]],
-  ['gltfmaterialxconverter_9',['glTFMaterialXConverter',['../classgltf__materialx__converter_1_1converter_1_1gl_t_f_material_x_converter.html',1,'gltf_materialx_converter::converter']]]
+  ['gltf_20procedurals_20interop_2',['MaterialX / glTF Procedurals Interop',['../index.html',1,'']]],
+  ['gltf_5fmaterialx_5fconverter_3',['gltf_materialx_converter',['../namespacegltf__materialx__converter.html',1,'']]],
+  ['gltf_5fmaterialx_5fconverter_3a_3a_5f_5fmain_5f_5f_4',['__main__',['../namespacegltf__materialx__converter_1_1____main____.html',1,'gltf_materialx_converter']]],
+  ['gltf_5fmaterialx_5fconverter_3a_3aconverter_5',['converter',['../namespacegltf__materialx__converter_1_1converter.html',1,'gltf_materialx_converter']]],
+  ['gltf_5fmaterialx_5fconverter_3a_3amaterialx_5fto_5fgltf_6',['materialx_to_gltf',['../namespacegltf__materialx__converter_1_1materialx__to__gltf.html',1,'gltf_materialx_converter']]],
+  ['gltf_5fmaterialx_5fconverter_3a_3autilities_7',['utilities',['../namespacegltf__materialx__converter_1_1utilities.html',1,'gltf_materialx_converter']]],
+  ['gltfmaterialxconverter_8',['glTFMaterialXConverter',['../classgltf__materialx__converter_1_1converter_1_1gl_t_f_material_x_converter.html',1,'gltf_materialx_converter::converter']]]
 ];
