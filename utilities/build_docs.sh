693e466c095978aa5a091c9b680457cf82294c4f
Bernard Kwok <kwokcb@gmail.com>
2024-10-16 14:39:58 -0400

    Check if replace string is being set.

--- utilities/build_docs.sh
+++ utilities/build_docs.sh
@@ -1,5 +1,5 @@
 python utilities/replace_string.py -s '$TOP' -r '.' utilities/README_template.md -o README.md
-python utilities/replace_string.py -rm=True utilities/README_template.md -o documents/README_api.md
+python utilities/replace_string.py -rm=1 utilities/README_template.md -o documents/README_api.md
 cd documents
 doxygen Doxyfile
 cd ..
