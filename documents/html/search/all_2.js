e4e40394e3bf4919e19ce091067bc9ca49bf207c
Bernard Kwok <kwokcb@gmail.com>
2024-10-17 16:37:18 -0400

    Update supported types to include: string, boolean, and filename (#21)

--- documents/html/search/all_2.js
+++ documents/html/search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['build_0',['Build',['../md__d_1_2work_2materialx_2gl_t_f-_material_x-_converter_2utilities_2_r_e_a_d_m_e.html#build',1,'']]],
-  ['build_20utilities_1',['Build Utilities',['../md__d_1_2work_2materialx_2gl_t_f-_material_x-_converter_2utilities_2_r_e_a_d_m_e.html',1,'']]]
+  ['build_0',['Build',['../md__d_1_2_work_2materialx_2gl_t_f-_material_x-_converter-_fork_2utilities_2_r_e_a_d_m_e.html#build',1,'']]],
+  ['build_20utilities_1',['Build Utilities',['../md__d_1_2_work_2materialx_2gl_t_f-_material_x-_converter-_fork_2utilities_2_r_e_a_d_m_e.html',1,'']]]
 ];
--- documents/html/search/all_d.js
+++ documents/html/search/all_d.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['read_5fmaterialx_5fdocument_0',['read_materialX_document',['../namespacegltf__materialx__converter_1_1utilities.html#afd53d89163491cd2b49952449b98617a',1,'gltf_materialx_converter::utilities']]],
   ['readme_5fapi_2emd_1',['README_api.md',['../_r_e_a_d_m_e__api_8md.html',1,'']]],
-  ['reference_20rendering_2',['Reference Rendering',['../md__d_1_2work_2materialx_2gl_t_f-_material_x-_converter_2utilities_2_r_e_a_d_m_e.html#reference-rendering',1,'']]],
-  ['rendering_3',['Reference Rendering',['../md__d_1_2work_2materialx_2gl_t_f-_material_x-_converter_2utilities_2_r_e_a_d_m_e.html#reference-rendering',1,'']]]
+  ['reference_20rendering_2',['Reference Rendering',['../md__d_1_2_work_2materialx_2gl_t_f-_material_x-_converter-_fork_2utilities_2_r_e_a_d_m_e.html#reference-rendering',1,'']]],
+  ['rendering_3',['Reference Rendering',['../md__d_1_2_work_2materialx_2gl_t_f-_material_x-_converter-_fork_2utilities_2_r_e_a_d_m_e.html#reference-rendering',1,'']]]
 ];
