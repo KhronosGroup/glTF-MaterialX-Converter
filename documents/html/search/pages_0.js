637a5c53152cbe9b211ae83d0d04f57c12a14e6c
Bernard Kwok <kwokcb@gmail.com>
2024-10-17 16:35:13 -0400

    Update supported types to include: string, boolean, and filename

--- documents/html/search/pages_0.js
+++ documents/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['build_20utilities_0',['Build Utilities',['../md__d_1_2work_2materialx_2gl_t_f-_material_x-_converter_2utilities_2_r_e_a_d_m_e.html',1,'']]]
+  ['build_20utilities_0',['Build Utilities',['../md__d_1_2_work_2materialx_2gl_t_f-_material_x-_converter-_fork_2utilities_2_r_e_a_d_m_e.html',1,'']]]
 ];
