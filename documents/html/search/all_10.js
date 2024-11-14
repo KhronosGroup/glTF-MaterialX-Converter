712d7b852b95aff71ae2dcd35c90e9fe2ec7b736
Bernard Kwok <kwokcb@gmail.com>
2024-11-14 08:56:38 -0500

    Add support to auto-generate names if none provided in the glTF file. Also handles if there are invalid / duplicate names in the glTF file by always generating a unique name.

--- documents/html/search/all_10.js
+++ documents/html/search/all_10.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['test_20data_0',['Test Data',['../index.html#test-data',1,'']]],
-  ['tests_1',['Tests',['../index.html#tests',1,'Tests'],['../md__d_1_2work_2materialx_2gl_t_f-_material_x-_converter-_fork_2tests_2_r_e_a_d_m_e.html',1,'Unit Tests']]],
+  ['tests_1',['Tests',['../index.html#tests',1,'Tests'],['../md__d_1_2_work_2materialx_2gl_t_f-_material_x-_converter-_fork_2tests_2_r_e_a_d_m_e.html',1,'Unit Tests']]],
   ['tests_2freadme_2emd_2',['README.md',['../tests_2_r_e_a_d_m_e_8md.html',1,'']]]
 ];
--- documents/html/search/all_11.js
+++ documents/html/search/all_11.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['unit_20tests_0',['Unit Tests',['../md__d_1_2work_2materialx_2gl_t_f-_material_x-_converter-_fork_2tests_2_r_e_a_d_m_e.html',1,'']]],
-  ['utilities_1',['Build Utilities',['../md__d_1_2work_2materialx_2gl_t_f-_material_x-_converter-_fork_2utilities_2_r_e_a_d_m_e.html',1,'']]],
+  ['unit_20tests_0',['Unit Tests',['../md__d_1_2_work_2materialx_2gl_t_f-_material_x-_converter-_fork_2tests_2_r_e_a_d_m_e.html',1,'']]],
+  ['utilities_1',['Build Utilities',['../md__d_1_2_work_2materialx_2gl_t_f-_material_x-_converter-_fork_2utilities_2_r_e_a_d_m_e.html',1,'']]],
   ['utilities_2epy_2',['utilities.py',['../utilities_8py.html',1,'']]],
   ['utilities_2freadme_2emd_3',['README.md',['../utilities_2_r_e_a_d_m_e_8md.html',1,'']]]
 ];
