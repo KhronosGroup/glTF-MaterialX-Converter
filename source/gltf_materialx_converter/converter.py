e9659d36525f39ed2cd38ecbf86a195c0897feae
Bernard Kwok <kwokcb@gmail.com>
2024-11-29 12:13:37 -0500

    Metadata update.

--- source/gltf_materialx_converter/converter.py
+++ source/gltf_materialx_converter/converter.py
@@ -209,7 +209,12 @@ class glTFMaterialXConverter():
         self.supported_types = ['boolean', 'string', 'integer', 'matrix33', 'matrix44', 'vector2', 'vector3', 'vector4', 'float', 'color3', 'color4']
         self.supported_scalar_types = ['integer', 'matrix33', 'matrix44', 'vector2', 'vector3', 'vector4', 'float', 'color3', 'color4']
         self.supported_array_types = ['matrix33', 'matrix44', 'vector2', 'vector3', 'vector4', 'color3', 'color4']
-        self.supported_metadata = ['colorspace', 'unit', 'unittype', 'uiname', 'uimin', 'uimax', 'uisoftmin', 'uisoftmax', 'uifolder', 'doc', 'xpos', 'ypos']
+        self.standard_ui_metadata = ['xpos', 'ypos', 'width', 'height', 'uicolor']
+        self.supported_metadata = ['colorspace', 'unit', 'unittype', 
+                                   'uiname', 'uimin', 'uimax', 'uisoftmin', 'uisoftmax', 'uistep', 'uifolder', 'uiadvanced', 'uivisible',
+                                   'defaultgeomprop', 'uniform', 
+                                   'doc'] + self.standard_ui_metadata
+        self.supported_graph_metadata = ['colorspace', 'unit', 'unittype', 'uiname', 'doc'] + self.standard_ui_metadata
 
     def set_debug(self, debug):
         '''
@@ -220,6 +225,13 @@ class glTFMaterialXConverter():
             self.logger.setLevel(lg.DEBUG)
         else:
             self.logger.setLevel(lg.INFO)
+
+    def get_standard_ui_metadata(self):
+        '''
+        Get the standard UI metadata defined by MaterialX.
+        @return The standard UI metadata.
+        '''
+        return self.standard_ui_metadata
 
     def set_metadata(self, metadata):
         '''
@@ -234,6 +246,13 @@ class glTFMaterialXConverter():
         @return The metadata.
         '''
         return self.supported_metadata
+
+    def get_graph_metadata(self):
+        '''
+        Get the supported graph metadata for the converter.
+        @return The metadata.
+        '''
+        return self.supported_graph_metadata
 
     def get_supported_target_type(self):
         '''
@@ -393,6 +412,12 @@ class glTFMaterialXConverter():
         procs.append(nodegraph)
 
         metadata = self.get_metadata()
+
+        # Set nodegraph metadata
+        graph_metadata = self.get_graph_metadata()
+        for meta in graph_metadata:
+            if graph.getAttribute(meta):
+                nodegraph[meta] = graph.getAttribute(meta)
 
         # Add nodes to to dictonary. Use path as this is globally unique
         #
@@ -1175,9 +1200,16 @@ class glTFMaterialXConverter():
             graph_name = doc.createValidChildName(graph_name)
             proc['name'] = graph_name
             
-            # Create new nodegraph
+            # Create new nodegraph and add metadata
             self.logger.info(f'> Create new nodegraph: {graph_name}')
             mtlx_graph = doc.addNodeGraph(graph_name)
+            graph_metadata= self.get_graph_metadata()
+            for meta in graph_metadata:
+                if meta in proc:
+                    proc_meta_data = proc[meta]
+                    self.logger.debug(f'> Add extra graph attribute: {meta}, {proc_meta_data}')
+                    mtlx_graph.setAttribute(meta, proc_meta_data)
+
             root_mtlx = mtlx_graph
 
             inputs = proc.get(KHR_TEXTURE_PROCEDURALS_INPUTS_BLOCK, [])
