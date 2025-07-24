53d486bb8da258f714bd9444b4713718427c1379
Bernard Kwok <kwokcb@gmail.com>
2025-07-24 16:37:00 -0400

    Make markdown a dev dependencie and check when calling build_docs.sh script.

--- utilities/build_docs.sh
+++ utilities/build_docs.sh
@@ -1,3 +1,4 @@
+pip install .[dev]
 python utilities/replace_string.py -s '$TOP' -r '.' utilities/README_template.md -o README.md
 python utilities/md2html.py README.md README.html
 python utilities/replace_string.py -rm=1 utilities/README_template.md -o documents/README_api.md
